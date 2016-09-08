d77180cc12151d0caea27b40aaeee5377e77f0f8
Brett New and Molly McGlone <brett+molly@>
2016-09-08 16:48:08 -0700

    add styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root :to => 'images#index'
+
   resources :stories do
     resources :contributions
   end
 
-  resources :images
+  resources :images, :only => [:show, :index]
 end
